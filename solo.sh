7ef98cd42ec78aa6f4582a5745c19b7f042914c0
Valmik Roy <valmikroy@users.noreply.github.com>
2020-01-27 13:41:56 -0800

    Update solo.sh

--- solo.sh
+++ solo.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sudo /opt/chef/embedded/bin/berks  vendor /tmp/chef-solo/cookbooks
 sudo chef-client --local-mode   -c solo.rb  -j solo.json
+# run it twice for bash_profiles to get create
+sudo chef-client --local-mode   -c solo.rb  -j solo.json
+

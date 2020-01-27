2dee41d481e9338a17f88ebf18e70623f88c0afe
valmikroy <valmikroy@gmail.com>
2020-01-26 18:40:57 -0800

    Updating default

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,21 +7,5 @@
 # Copyright 2020, Valmik Roy
 # All rights reserved - Do Not Redistribute
 
-motd '99-mymotd'
-
-user node.normal['username'] do
-  manage_home true
-  comment node.normal['username']
-  home "/home/#{node.normal['username']}"
-  shell '/bin/bash'
-end
-
-sudo node.normal['username'] do
-  user      node.normal['username']
-  nopasswd  true
-end
-
-# ssh_authorize_key  node.normal['username'] do
-#  key  node.normal['user_pub_key']
-#  user node.normal['username']
-# end
+include_recipe "workstation-bootstrap::user"
+include_recipe "workstation-bootstrap::tmux"

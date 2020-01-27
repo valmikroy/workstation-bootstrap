cd381b1a9f3f11bc70fb7b9a53b01117c0ee0727
valmikroy <valmikroy@gmail.com>
2020-01-26 17:50:30 -0800

    Run first.sh to create a user

--- first.sh
+++ first.sh
@@ -4,13 +4,14 @@ apt-get -y update
 apt-get -y install build-essential ruby-dev git curl build-essential libxml2-dev libxslt-dev libssl-dev autoconf
 
 if [ ! -e /opt/chef/bin/chef-solo ] ; then
-    curl -L https://www.opscode.com/chef/install.sh | bash
+    curl -L https://www.opscode.com/chef/install.sh | sudo bash
 fi
 if [ ! -e /opt/chef/embedded/bin/berks ] ; then
-    /opt/chef/embedded/bin/gem install berkshelf --no-ri --no-rdoc
+    sudo /opt/chef/embedded/bin/gem install berkshelf 
     ln -s /opt/chef/embedded/bin/berks /usr/local/bin/berks
 fi
 
 
-/opt/chef/embedded/bin/berks  vendor /tmp/chef-solo/cookbooks
-sudo chef-client --local-mode   -c solo.rb  -j solo.json
+sudo /opt/chef/embedded/bin/berks  vendor /tmp/chef-solo/cookbooks
+sudo chef-client --local-mode   -c solo.rb  -j first.json
+rm -rf  /tmp/chef-solo

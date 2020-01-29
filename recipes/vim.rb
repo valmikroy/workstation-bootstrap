cad3198f7b7a2ff0fe5a23532ba0772c70e575f1
Abhishek Sawant <abhsawan@justin.tv>
2020-01-29 12:40:04 -0800

    Adding VIM

--- recipes/vim.rb
+++ recipes/vim.rb
@@ -1,11 +1,31 @@
-apt_repository 'neovim' do
-    uri        'ppa:neovim-ppa/stable'
-      components ['neovim']
+
+
+script 'extract_module' do
+  interpreter "bash"
+  code <<-EOH
+	 sudo add-apt-repository ppa:neovim-ppa/stable
+	 sudo apt-get update
+	 sudo apt-get install neovim
+
+	 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+	 sudo update-alternatives --config vi
+	 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+	 sudo update-alternatives --config vim
+	 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+	 sudo update-alternatives --config editor
+  EOH
 end
 
 
 
-package 'neovim'
+#apt_repository 'neovim' do
+#    uri        'ppa:neovim-ppa/stable'
+#      components ['neovim']
+#end
+#
+#
+## this does not work on Xenial as expected - fix it later
+#package 'neovim'
 
 if ::File.exist?("#{node['tmp_path']}/vim.source")
 
@@ -14,4 +34,10 @@ if ::File.exist?("#{node['tmp_path']}/vim.source")
     content  IO.read("#{node['tmp_path']}/vim.source")
   end
 
+end
+
+
+directory "#{node['user_home']}/.local/share/nvim/site/autoload" do
+  action :create
+  recursive true
 end

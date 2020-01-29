6db828c9fedbb9076e2d73822c4fa2c53e6d40ce
valmikroy <valmikroy@gmail.com>
2020-01-29 13:21:03 -0800

    Adding user

--- solo.sh
+++ solo.sh
@@ -4,5 +4,7 @@ sudo chef-client --local-mode   -c solo.rb  -j solo.json
 # run it twice for bash_profiles to get create
 sudo chef-client --local-mode   -c solo.rb  -j solo.json
 
-chown -R $USER:$USER  $HOME
+
+
+chown -R $SUDO_USER:$SUDO_USER  $HOME
 

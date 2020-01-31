2e7fd1624d436f423940bd58a51fe20fe03d270b
valmikroy <valmikroy@gmail.com>
2020-01-30 17:17:13 -0800

    rolling back

--- recipes/python.rb
+++ recipes/python.rb
@@ -9,12 +9,9 @@ end
 python_virtualenv "#{node['user_home']}/env/python3" do
 	 user node['username']
 	 python 'python3'
-     pip_version true
-     setuptools_version true
-
 	 action :create
 end
 
-python_package 'pynvim' do
-    virtualenv "#{node['user_home']}/env/python3"
-end
+#python_package 'pynvim' do
+#    virtualenv "#{node['user_home']}/env/python3"
+#end

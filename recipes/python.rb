d8a884984c0df4c8be24b6e6c81737a31617403e
valmikroy <valmikroy@gmail.com>
2020-01-30 16:54:00 -0800

    python virtual env

--- recipes/python.rb
+++ recipes/python.rb
@@ -1,3 +1,14 @@
 #package 'python3-dev'
 #package 'python3-pip'
-python_package 'pynvim'
+#python_package 'pynvim'
+
+python_runtime 'python3' do
+	version '3.5'
+	action :install
+end
+
+python_virtualenv "#{node['user_home']}/env/python3" do
+	 user node['username']
+	 python 'python3'
+	 action :create
+end

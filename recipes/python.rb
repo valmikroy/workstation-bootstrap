a0ee05ba7060b48c186f8146019198f370832557
valmikroy <valmikroy@gmail.com>
2020-01-30 16:59:59 -0800

    Adding pip pkg

--- recipes/python.rb
+++ recipes/python.rb
@@ -1,6 +1,5 @@
 #package 'python3-dev'
 #package 'python3-pip'
-#python_package 'pynvim'
 
 python_runtime 'python3' do
 	version '3.5'
@@ -11,4 +10,8 @@ python_virtualenv "#{node['user_home']}/env/python3" do
 	 user node['username']
 	 python 'python3'
 	 action :create
+end
+
+python_package 'pynvim' do
+    python 'python3'
 end

aabf15beb0b89f3e4b80ad607ea5ea610f401bfb
Valmik Roy <valmikroy@users.noreply.github.com>
2020-02-02 10:33:52 -0800

    Update vim.rb

--- recipes/vim.rb
+++ recipes/vim.rb
@@ -1,33 +1,19 @@
-
-#	 sudo add-apt-repository ppa:neovim-ppa/stable
-#	 sudo apt-get update
-#	 sudo apt-get install neovim
-#
-
 script 'neovim system settings update' do
   interpreter "bash"
   code <<-EOH
-	 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-	 sudo update-alternatives --config vi
-	 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
-	 sudo update-alternatives --config vim
-	 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
-	 sudo update-alternatives --config editor
+
+     curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+     chmod +x nvim.appimage
+     mv nvim.appimage  /usr/bin/nvim
+         sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+         sudo update-alternatives --config vi
+         sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+         sudo update-alternatives --config vim
+         sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+         sudo update-alternatives --config editor
   EOH
-  only_if { ::File.exist?("/usr/bin/nvim") }
+  not_if { ::File.exist?("/usr/bin/nvim") }
 end
-
-
-
-#apt_repository 'neovim' do
-#    uri        'ppa:neovim-ppa/stable'
-#      components ['neovim']
-#end
-#
-#
-## this does not work on Xenial as expected - fix it later
-package 'neovim'
-package 'neovim-runtime'
 
 if ::File.exist?("#{node['tmp_path']}/vim.source")
 
@@ -39,12 +25,10 @@ if ::File.exist?("#{node['tmp_path']}/vim.source")
 end
 
 
-#directory "#{node['user_home']}/.local/share/nvim/site/autoload" do
-#  action :create
-#  recursive true
-#end
-
-
+directory "#{node['user_home']}/.local/share/nvim/site/autoload" do
+  action :create
+  recursive true
+end
 
 directory "#{node['user_home']}/.config/nvim/" do
   action :create
@@ -52,17 +36,13 @@ directory "#{node['user_home']}/.config/nvim/" do
 end
 
 cookbook_file 'neovim_config' do
-	source 'vim/init.vim'
-	path "#{node['user_home']}/.config/nvim/init.vim"
+        source 'vim/init.vim'
+        path "#{node['user_home']}/.config/nvim/init.vim"
 end
-
-
-
-
 
 script 'neovim plug install' do
   interpreter "bash"
   code <<-EOH
-	 curl -fLo #{node['user_home']}/.local/share/nvim/site/autoload/plug.vim  --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+         curl -fLo #{node['user_home']}/.local/share/nvim/site/autoload/plug.vim  --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   EOH
 end

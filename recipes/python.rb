beec6de2b6a285aac017c80df34db7ca0a550bfb
Valmik Roy <valmikroy@users.noreply.github.com>
2020-02-01 16:03:27 -0800

    Update python.rb

--- recipes/python.rb
+++ recipes/python.rb
@@ -1,40 +1,42 @@
-#package 'python3-dev'
-#package 'python3-pip'
+package 'python3.8'
+package 'python3.8-dev'
+package 'python3.8-minimal'
+package 'python3.8-venv'
 
 #apt_repository 'python' do
 #    uri        'ppa:jonathonf/python-3.6'
 #    components ['python']
 #end
-python_runtime 'python3' do
-	version '3.5'
-	action :install
-end
+#python_runtime 'python3' do
+#       provider :system
+#       options package_name: 'python3.8'
+#       version '3.8'
+#       action :install
+#end
 
 #python_runtime_options 'python3' do
 #    version '3.5'
 #end
 
 #python_virtualenv "#{node['user_home']}/env/python3" do
-#	 user node['username']
-#	 python_runtime "#{node['user_home']}/env/python3/bin/python"
+#        user node['username']
+#        python_runtime "#{node['user_home']}/env/python3/bin/python"
      #setuptools_version false
      #pip_version false
      #wheel_version false
-#	 action :create
+#        action :create
 #end
 
-python_package 'pynvim' 
+#python_package 'pynvim'
 #python_package 'pynvim' do
 #    virtualenv "#{node['user_home']}/env/python3"
 #end
 
-#script 'install pips' do
-#  interpreter "bash"
-#  code <<-EOH
-#   "#{node['user_home']}/env/python3/bin/python -m pip install --user --upgrade pip"
-#   "#{node['user_home']}/env/python3/bin/pip install setuptools"
-#   "#{node['user_home']}/env/python3/bin/pip install wheel"
-#   "#{node['user_home']}/env/python3/bin/pip install pynvim"
-#  EOH
-#  not_if { ::File.exist?("#{node['user_home']}/env/python3/bin/python") }
-#end
+script 'install pips' do
+  interpreter "bash"
+  code <<-EOH
+   "python3.8 -m pip install --user virtualenv"
+   "python3.8 -m venv #{node['user_home']}/env"
+  EOH
+  not_if { ::File.exist?("/usr/bin/python3.8") }
+end

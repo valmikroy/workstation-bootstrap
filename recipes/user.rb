734a1a8db46460083bc3778c0ff38dacbd680eeb
abhisawa <abhisawa@ip-172-31-10-229.us-west-1.compute.internal>
2020-02-03 20:45:55 +0000

    two keys model

--- recipes/user.rb
+++ recipes/user.rb
@@ -21,8 +21,13 @@ sudo node['username'] do
   nopasswd  true
 end
 
-ssh_authorize_key node['username'] do
-  key  node['user_pub_key']
+ssh_authorize_key "#{node['username']}_1" do
+  key  node['user_pub_key1']
+  user node['username']
+  only_if "getent passwd #{node['username']}"
+end
+ssh_authorize_key "#{node['username']}_2" do
+  key  node['user_pub_key2']
   user node['username']
   only_if "getent passwd #{node['username']}"
 end

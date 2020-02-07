fc7b7311d54cc17c8601ce72bf86676c5d1255de
Valmik Roy <valmikroy@users.noreply.github.com>
2020-02-07 09:29:56 -0800

    Update test_test.go

--- files/default/vim/templates/golang/test_test.go
+++ files/default/vim/templates/golang/test_test.go
@@ -1,8 +1,15 @@
 package main
 
-import "testing"
+import (
+        //"fmt"
+        "io/ioutil"
+        "strings"
+        "testing"
+)
 
-func TestHello(t *testing.T) {
+
+
+func TestStringInput(t *testing.T) {
 
         assertCorrectMessage := func(t *testing.T, input string, desired_out []string) {
                 t.Helper()
@@ -24,5 +31,33 @@ func TestHello(t *testing.T) {
                         "hello Abhishek3",
                 }
                 assertCorrectMessage(t, input, desired_out)
+        })
+}
+
+func TestFileInput(t *testing.T) {
+
+        assertCorrectMessage := func(t *testing.T, content []byte, desired_out []string) {
+                t.Helper()
+
+                first_line := strings.Split(string(content), "\n")[0]
+                first_word := strings.Fields(first_line)[0]
+                var d *Data = NewData(first_word)
+                for _, out := range desired_out {
+                        got := d.hello()
+                        if got != out {
+                                t.Error("Expected output ", out, " but got ", got)
+                        }
+                }
+        }
+
+        t.Run("First test", func(t *testing.T) {
+                t.Logf("second type")
+                content, _ := ioutil.ReadFile("resources/F1_fru.out")
+                var desired_out = []string{
+                        "hello Abhishek1",
+                        "hello Abhishek2",
+                        "hello Abhishek3",
+                }
+                assertCorrectMessage(t, content, desired_out)
         })
 }

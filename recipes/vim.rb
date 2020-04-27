7aa4f000bfb1357574de391027c8b3703aa7a72c
abhisawa <abhisawa@ip-172-31-26-143.us-west-1.compute.internal>
2020-04-27 03:40:11 +0000

    adding coc settings

--- recipes/vim.rb
+++ recipes/vim.rb
@@ -4,7 +4,13 @@ cookbook_file 'neovim_config' do
         path "#{node['user_home']}/.config/nvim/init.vim"
 end
 
-directory "#{node['user_home']}/.config/nvim/plugged/vim-go/templates" do
+
+#directory "#{node['user_home']}/.config/nvim/plugged/vim-go/templates" do
+#  owner node['username']
+#  recursive true
+#end
+
+directory "#{node['user_home']}/.config/coc/ultisnips" do
   owner node['username']
   recursive true
 end
@@ -25,14 +31,26 @@ cookbook_file 'vim_snip_golang' do
   path "#{node['user_home']}/.config/nvim/UltiSnips/go.snippets"
 end
 
-cookbook_file 'vim_snip_snippets1' do
+cookbook_file 'vim_snip_golang' do
   user node['username']
-  source 'vim/vim_go_template/hello_world.go'
-  path "#{node['user_home']}/.config/nvim/plugged/vim-go/templates/hello_world.go"
+  source 'vim/ultisnips/go.snippets'
+  path "#{node['user_home']}/.config/coc/ultisnips/go.snippets"
 end
 
-cookbook_file 'vim_snip_snippets2' do
+cookbook_file 'coc-settings' do
   user node['username']
-  source 'vim/vim_go_template/hello_world_test.go'
-  path "#{node['user_home']}/.config/nvim/plugged/vim-go/templates/hello_world_test.go"
+  source 'vim/coc-settings.json'
+  path "#{node['user_home']}/.config/nvim/coc-settings.json"
 end
+
+#cookbook_file 'vim_snip_snippets1' do
+#  user node['username']
+#  source 'vim/vim_go_template/hello_world.go'
+#  path "#{node['user_home']}/.config/nvim/plugged/vim-go/templates/hello_world.go"
+#end
+
+#cookbook_file 'vim_snip_snippets2' do
+#  user node['username']
+#  source 'vim/vim_go_template/hello_world_test.go'
+#  path "#{node['user_home']}/.config/nvim/plugged/vim-go/templates/hello_world_test.go"
+#end
